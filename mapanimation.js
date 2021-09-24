7399b8357bf71e8aebd9eb0b47ee63cd31b2020d
Derek D <87330803+ForeverPhoenix21@users.noreply.github.com>
2021-09-24 12:22:38 -0400

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -22,7 +22,7 @@ async function run() {
 
 // Request bus data from MBTA
 async function getBusLocations() {
-  const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
+  const url = "https://api-v3.mbta.com/vehicles?filter[route]=1,7&include=trip";
   const response = await fetch(url);
   const json = await response.json();
   return json.data;
